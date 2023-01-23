1ef22f14af3c840c9c00aaf854bceee2e3d2c5d3
Trevon Lootus <u-481@tptlive.ee>
2023-01-23 15:42:16 +0200

    Add express server and nodemon

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const app = require('path')
+const path = require('path')
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile (path.resolve(_dirname, 'index.html'));
+  res.sendFile (path.resolve(__dirname, 'index.html'));
   //console.log(path.resolve(_dirname, 'index.html'));
 });
 
